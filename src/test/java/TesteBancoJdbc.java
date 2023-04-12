8478048b4e719f558b7d815d7b11acc3d6aedf07
Pedro Henrique <pedro.henrique59@outlook.com>
2023-04-12 13:51:05 -0300

    Criando conexão com o banco.

---
+++ src/test/java/TesteBancoJdbc.java
@@ -0,0 +1,11 @@
+import conexaojdbc.SingleConnection;
+import org.junit.Test;
+
+public class TesteBancoJdbc {
+
+    @Test
+    public void initBanco() {
+        SingleConnection.getConnection();
+    }
+
+}
