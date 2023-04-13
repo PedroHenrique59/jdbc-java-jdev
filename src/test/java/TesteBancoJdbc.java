6809d5aa196ade77907bf4172f2f2327d99573c5
Pedro Henrique <pedro.henrique59@outlook.com>
2023-04-13 11:21:02 -0300

    Criando Testes.

--- src/test/java/TesteBancoJdbc.java
+++ src/test/java/TesteBancoJdbc.java
@@ -1,11 +1,35 @@
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.UserPosJava;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TesteBancoJdbc {
 
     @Test
     public void initBanco() {
-        SingleConnection.getConnection();
+        //insertTest();
+        obterTodosTest();
+        obterPorIdTest();
     }
 
+    public void insertTest() {
+        UserPosDAO userPosDAO = new UserPosDAO();
+        UserPosJava userPosJava = new UserPosJava(2L, "Pedro", "pedro@gmail.com");
+        userPosDAO.salvar(userPosJava);
+    }
+
+    public void obterTodosTest() {
+        UserPosDAO userPosDAO = new UserPosDAO();
+        List<UserPosJava> users = userPosDAO.obterTodos();
+        for (UserPosJava user : users) {
+            System.out.println(user);
+        }
+    }
+
+    public void obterPorIdTest() {
+        UserPosDAO userPosDAO = new UserPosDAO();
+        UserPosJava userPosJava = userPosDAO.obterPorId(1L);
+        System.out.println("Obter por id: " + userPosJava);
+    }
 }
